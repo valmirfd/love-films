338f4400c8b2e30352cbd9c52a762d1814c7ce41
Valmir Dornas <fernandocursos2021@gmail.com>
2025-08-26 12:16:22 -0300

    Theme button

---
+++ app/src/main/java/com/valorizeseusamigos/lovemovies/presenter/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.valorizeseusamigos.lovemovies.presenter
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.valorizeseusamigos.lovemovies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/valorizeseusamigos/lovemovies/presenter/onboarding/OnboardingFragment.kt
@@ -0,0 +1,32 @@
+package com.valorizeseusamigos.lovemovies.presenter.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.valorizeseusamigos.lovemovies.databinding.FragmentOnboardingBinding
+
+
+
+class OnboardingFragment : Fragment() {
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
